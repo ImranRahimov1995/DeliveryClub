6c5e4a4270f929b1a96f2395c51a111df846ff4f
ImranRahimov1995 <imash.odessahouse@gmail.com>
2022-07-30 01:26:27 +0400

    Add interface to delivery app.
    1. Add endpoint to urls http://0.0.0.0
    2. Create handler which get all records list and other data.
    3. Create html template with tables and chart
    4. Add css file.

--- project/apps/delivery_club/views.py
+++ project/apps/delivery_club/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.db.models import Sum
 
-# Create your views here.
+from apps.delivery_club.models import DeliveryRecord
+
+
+class DeliveryRecordsDashboard(ListView):
+    # Сортируем по row id для точного повторения таблицы
+    queryset = DeliveryRecord.objects.all().order_by('row_id')
+    template_name = 'index.html'
+    context_object_name = 'records'
+
+    def get_context_data(self, *args, **kwargs):
+        """
+            Записываем в контекст дополнительно:
+             Сумму в долларах, рублях, отдельно даты и цены для графика.
+        """
+        context = super().get_context_data(*args, **kwargs)
+
+        context['total_in_usd'] = DeliveryRecord.objects.aggregate(
+            Sum('price')
+        )['price__sum']
+
+        context['total_in_rub'] = DeliveryRecord.objects.aggregate(
+            Sum('price_in_rub')
+        )['price_in_rub__sum']
+
+        _ = DeliveryRecord.objects.order_by('-delivery_date').values_list(
+            'delivery_date', "price"
+        )
+        _dates = []
+        _prices = []
+
+        for _date, _price in _:
+            _dates.append(_date.strftime("%d.%m.%Y"))
+            _prices.append(str(_price))
+
+        context['dates'] = _dates
+        context['prices'] = _prices
+
+        return context
--- project/config/urls.py
+++ project/config/urls.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from apps.delivery_club.views import DeliveryRecordsDashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('', DeliveryRecordsDashboard.as_view(), name='dashboard')
 ]
-
 
 # Local settings
 if settings.DEBUG:
